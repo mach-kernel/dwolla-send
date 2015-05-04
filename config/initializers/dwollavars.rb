4f20a6706dd621da522a3a249815768a8af1b199
David Stancu <ds4028@nyu.edu>
2015-05-03 20:11:46 -0400

    add my UAT creds

--- config/initializers/dwollavars.rb
+++ config/initializers/dwollavars.rb
@@ -8,8 +8,8 @@ module DwollaVars
 	@@Dwolla::api_key ||= "bXTSJDHveBHltmu95EM7a6gt6zdRmPJnQgJP774ev+Bc02sEnf"
 	@@Dwolla::api_secret ||= "v0OG0MCM9eEHeNuC/QAQ89B6s2VcwyjEHgJuBd0e6Lp2sUyIkU"
 
-	@@Dwolla::token ||= nil
-	@@pin ||= nil
+	@@Dwolla::token ||= "q58Z92xpws58yKn9iWW77sOnsSGL3QDIOCo1c4zLtrOxxuCoZW"
+	@@pin ||= 1337
 
 	@@Dwolla::sandbox ||= true
 end
